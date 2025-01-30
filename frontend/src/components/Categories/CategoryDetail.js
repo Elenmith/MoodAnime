facf960636fab07cc6a4d1ec376dcfe8a70e5dba
Elenmith <123502446+Elenmith@users.noreply.github.com>
2025-01-30 08:54:46 +0100

    Update CategoryDetail.js

--- frontend/src/components/Categories/CategoryDetail.js
+++ frontend/src/components/Categories/CategoryDetail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./CategoryDetail.css";
 
 const CategoryDetail = () => {
