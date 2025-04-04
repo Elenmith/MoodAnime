5269f2283c2a9911ce5125229102871a637389c1
Elenmith <123502446+Elenmith@users.noreply.github.com>
2025-04-04 21:04:06 +0200

    Add files via upload

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <div className="about-content">
+        <h1>
+          Hi! If you want to contact us, you can use any of the links below, or
+          write to me directly on email:{" "}
+        </h1>
+        <a href="https:/visiblify.com" target="_blank" rel="noreferrer">
+          Visiblify
+        </a>
+        <br></br>
+        <a href="https:/github.com/Elenmith" target="_blank" rel="noreferrer">
+          Github
+        </a>
+        <p>radekkowalczyk247@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
